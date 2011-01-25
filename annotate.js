f461888074fda06619df7d5e353e10da10e72379
paolo <paolo@paolo-vm.(none)>
2011-01-25 16:22:51 -0200

    Annotation coordinates parameter is now a map

--- annotate.js
+++ annotate.js
@@ -1,45 +1,68 @@
+/**
+ * CSS class configured for annotation DIVs
+ */ 
 var Annotate_ANNOTATION_CLASS = 'annotation';
+
+/**
+ * In order to put the image inside a DIV, I have to know the image's dimensions.
+ * The problem is that you don't have this information until the image is fully
+ * loaded. To bypass that, I enqueue all 'annotate' operations on different stacks
+ * for each image, then changes the images' 'onload' parameter to point to a
+ * function that dequeues and executes each function.
+ */
 var Annotate_op_queue = {};
 
+/**
+ * Shortcut for 'document.getElementById'
+ */ 
 function $(id) {
     return document.getElementById(id);
 }
 
+/**
+ * Pushes an operation 'fn' in the operation stack identified by 'id'
+ */
 function _enqueue_operation(id, fn) {
     if(Annotate_op_queue[id] == undefined) {
        Annotate_op_queue[id] = new Array(); 
     }
     Annotate_op_queue[id].push(fn);
-}
+} 
 
 function _flush_operation_queue(id) {
     if(Annotate_op_queue[id] != undefined) {
         while(Annotate_op_queue[id].length > 0) {
-            Annotate_op_queue[id].pop()();
+            Annotate_op_queue[id].pop()(); // Yeah, it's actually a stack. Sue me.
         }
     }
 }
 
-function _annotate(id, annotation_id, message, width, height) {
-    annotate(id, annotation_id, message, width, height, 0, 0);
-}
-
-function _annotate(id, annotation_id, message, width, height, x, y) {        
-    container = _get_container(id);
-    node = $(id);
+function _annotate(id, annotation_id, message, area_data) {        
+    var container = _get_container(id);
+    var node = $(id);
    
     annotation = document.createElement('div');        
     
     annotation.id = annotation_id;
     annotation.setAttribute('class', Annotate_ANNOTATION_CLASS); 
-    
     annotation.style.display = 'none';
     annotation.style.position = 'absolute';
-    annotation.style.left = x;
-    annotation.style.top = y;
-    annotation.style.width = width;
-    annotation.style.height = height;
-    //annotation.style.zIndex = container.childNodes.length + 1;
+
+    if(area_data['x']) {
+        annotation.style.left = area_data['x'];
+    }
+    
+    if(area_data['y']) {
+        annotation.style.top = area_data['y'];
+    }
+
+    if(area_data['w']) {
+        annotation.style.width = area_data['w'];
+    }
+
+    if(area_data['h']) {
+        annotation.style.height = area_data['h'];
+    }
     
     annotation.innerHTML = message;
     annotation.alt = message;
@@ -48,10 +71,12 @@ function _annotate(id, annotation_id, message, width, height, x, y) {
 }
 
 function _get_container(id) {
-    cid = "#" + id;
-    container = $(cid);     
+    var cid = "#" + id;
+    var container = $(cid);     
 
     if(container == undefined) {
+        var node = $(id);
+
         container = document.createElement('div');
         container.id = cid;
         container.style.backgroundImage = 'url(' + node.src + ')';
@@ -61,7 +86,6 @@ function _get_container(id) {
         container.onmouseover = function(){_show_annotations(cid)};
         container.onmouseout = function(){_hide_annotations(cid)};
 
-        node = $(id);
         node.parentNode.replaceChild(container, node);
     }
 
@@ -69,7 +93,7 @@ function _get_container(id) {
 }
 
 function _show_annotations(container_id) {
-    var children = $(container_id).childNodes;
+    children = $(container_id).childNodes;
     for(var i = 0; i < children.length; i++) {
         children[i].style.display = 'block';
     }
@@ -82,12 +106,7 @@ function _hide_annotations(container_id) {
     }
 }
 
-function annotate(id, annotation_id, message, width, height) {       
-    _enqueue_operation(id, function(){_annotate(id, annotation_id, message, width, height)});
-    $(id).onload = function(){_flush_operation_queue(id)};
-}
-
-function annotate(id, annotation_id, message, width, height, x, y) {
-    _enqueue_operation(id, function(){_annotate(id, annotation_id, message, width, height, x, y)});
+function annotate(id, annotation_id, message, area_data) {       
+    _enqueue_operation(id, function(){_annotate(id, annotation_id, message, area_data)});
     $(id).onload = function(){_flush_operation_queue(id)};
 }
